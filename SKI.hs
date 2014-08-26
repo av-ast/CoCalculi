b8a5a2e0a76fea50365afb7f96d3f01c15170f80
Alexey Astafyev <av.astafyev@gmail.com>
2014-08-26 11:09:48 +0400

    added combinator transformer to SKI basis

---
+++ SKI.hs
@@ -0,0 +1,45 @@
+data Combinator = Var String
+                | App Combinator Combinator
+                | Lambda String Combinator
+                deriving Eq
+
+instance Show Combinator where
+  show (Var x)      = x
+  show (App x y)    =
+    case y of
+      App _ _ -> showLambda x ++ "(" ++ show y ++ ")"
+      _       -> showLambda x ++ showLambda y
+    where
+      showLambda l@(Lambda _ _) = "(" ++ show l ++ ")"
+      showLambda x              = show x
+  show (Lambda x e) = "\\" ++ x ++ "." ++ show e
+
+-- SKI basis
+--
+s = Var "S"
+k = Var "K"
+i = Var "I"
+
+free :: String -> Combinator -> Bool
+free x (Var y)      = x == y
+free x (App e1 e2)  = free x e1 || free x e2
+free x (Lambda y e) | x /= y = free x e
+                    | x == y = False
+
+transform :: Combinator -> Combinator
+transform (Var x)                   = Var x
+transform (App x y)                 = App (transform x) (transform y)
+transform (Lambda x (Var y))        | x == y = i
+                                    | otherwise = App k (Var y)
+transform (Lambda x l@(Lambda _ _)) = transform (Lambda x (transform l))
+transform (Lambda x (App e1 e2))    = App (App s (transform (Lambda x e1)))
+                                                 (transform (Lambda x e2))
+
+-- other useful combinators
+--
+b = Lambda "x" (Lambda "y" (Lambda "z" (App (Var "x") (App (Var "y") (Var "z")))))
+c = Lambda "x" (Lambda "y" (Lambda "z" (App (Var "x") (App (Var "z") (Var "y"))) ))
+w = Lambda "x" (Lambda "y" (App (Var "x") (App (Var "y") (Var "y"))))
+
+-- y =
+
